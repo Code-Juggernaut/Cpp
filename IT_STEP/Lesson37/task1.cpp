362b66b1c7e8fc699692e073ac4c1f1d79183865
Andrii <andriyvasyliuk456@gmail.com>
2025-12-04 18:55:05 +0200

    some changes

--- IT_STEP/Lesson37/task1.cpp
+++ IT_STEP/Lesson37/task1.cpp
@@ -2,6 +2,12 @@
 #include<vector>
 #include<iterator>
 
-int main(){
+using namespace std;
 
+int main(){
+    const int init[] = {1,2,3,4,5};
+    vector<int> v1(5);
+    copy(init,init+5,v1.begin());
+    copy(v1.begin(),v1.end(),ostream_iterator<int>(cout," "));
+    copy(v1.rbegin(),v1.rend(),ostream_iterator<int>(cout," "));
 }
